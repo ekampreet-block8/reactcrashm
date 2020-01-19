f5b70878e721396388a8dbce3e173b2cf29cadf9
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-20 00:51:22 +0530

    passing argument general fails compilation no pass

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -15,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
