b5dbb01bbac44ef9fe586d62cd71dbad6ff69a2a
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-20 01:08:15 +0530

    ---

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,8 +5,7 @@ class Counter extends Component {
     count: this.props.value
   };
 
-  handleIncrement = product => {
-    console.log(product);
+  handleIncrement = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -15,7 +14,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
